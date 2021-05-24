9ff861cdf5e949da725651854a00ed39ea41b86d
alx.korolenok@gmail.com <alx.korolenok@gmail.com>
2021-05-24 12:11:23 +1000

    Добавлен клиент

---
+++ SomeClient/LocalAppSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeClient
+{
+    public class LocalAppSettings
+    {
+        public string Header { get; set; } = "";
+        public double Height { get; set; } = 0;
+        public double Width { get; set; } = 0;
+        public int Top { get; set; } = 0;
+        public int Left { get; set; } = 0;
+        public int Right { get; set; } = 0;
+    }
+}
---
+++ SomeClient/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using SettingsNetComponent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SomeClient
+{
+    public partial class MainWindow : Window
+    {
+        public LocalAppSettings AppSettings { get; set; } = new LocalAppSettings();
+        ISettingsProvider<LocalAppSettings> SettingsProvider { get; set; } =
+            new SettingsProvider<LocalAppSettings>(new NetTcpJsonProvider(
+                new AppIdentifier() { Id = Assembly.GetExecutingAssembly().GetName().Name },
+                new Uri($@"net.tcp://192.168.221.198:15001/SettingsService"))
+        );
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppSettings = new LocalAppSettings();
+            this.DataContext = AppSettings;
+            this.Title = Assembly.GetExecutingAssembly().GetName().Name;
+        }
+
+        private void ButtonLoadSettings(object sender, RoutedEventArgs e)
+        {
+            AppSettings = SettingsProvider.Load();
+            this.DataContext = AppSettings;
+        }
+
+        private void ButtonSaveSettings(object sender, RoutedEventArgs e)
+        {
+            SettingsProvider.Save(AppSettings);
+        }
+    }
+}
